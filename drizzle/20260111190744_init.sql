6ac5f6e82b30626810583e2df5efe0c1a72dbf1a
abdullahsafwan <abdullahsafwan768@gmail.com>
2026-01-12 00:08:50 +0500

    add posts table and update users table schema

---
+++ drizzle/20260111190744_init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "posts" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"content" varchar(5000) NOT NULL,
+	"authorId" uuid NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"age" integer NOT NULL,
+	"email" varchar(255) NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "posts" ADD CONSTRAINT "posts_authorId_users_id_fk" FOREIGN KEY ("authorId") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,17 +1,17 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, uuid } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid().primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
 
-const postsTable = pgTable("posts", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+export const postsTable = pgTable("posts", {
+  id: uuid().primaryKey().defaultRandom(),
   title: varchar({ length: 255 }).notNull(),
   content: varchar({ length: 5000 }).notNull(),
-  authorId: integer().notNull().references(() => usersTable.id),
+  authorId: uuid().notNull().references(() => usersTable.id),
 });
 
 export const schema = {
